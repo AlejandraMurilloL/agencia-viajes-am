3963442779b9283d7c652d335dadfb9c7295e16a
Maria Alejandra Murillo <malemurillo15@gmail.com>
2023-10-24 19:25:30 -0500

    Se agregan componentes base de las paginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'hoteles'
+    },
+    {
+        path: 'hoteles',
+        loadComponent: () => import('./features/hotels/hotels.component').then(comp => comp.HotelsComponent)
+    },
+    {
+        path: 'habitaciones',
+        loadComponent: () => import('./features/rooms/rooms.component').then(comp => comp.RoomsComponent)
+    },
+    {
+        path: 'reservas',
+        loadComponent: () => import('./features/reservations/reservations.component').then(comp => comp.ReservationsComponent)
+    }
+];
