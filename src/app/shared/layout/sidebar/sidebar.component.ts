4f16fc02ae0dafbba74be3d976c8415a32f2c0fa
Maria Alejandra Murillo <malemurillo15@gmail.com>
2023-10-24 19:17:24 -0500

    Se agrega sidebar

---
+++ src/app/shared/layout/sidebar/sidebar.component.ts
@@ -0,0 +1,50 @@
+import { Component, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterOutlet } from '@angular/router';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { BreakpointObserver } from '@angular/cdk/layout';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, MatSidenavModule, MatIconModule, MatToolbarModule, MatListModule, MatButtonModule],
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+  @ViewChild(MatSidenav) sidenav!: MatSidenav;
+
+  title = 'agencia-viajes';
+  isCollapsed = false;
+  isMobile= true;
+
+  constructor(private observer: BreakpointObserver, private router: Router) {}
+
+  ngOnInit() {
+    this.observer.observe(['(max-width: 800px)']).subscribe((screenSize) => {
+      if(screenSize.matches){
+        this.isMobile = true;
+      } else {
+        this.isMobile = false;
+      }
+    });
+  }
+
+  toggleMenu() {
+    if(this.isMobile){
+      this.sidenav.toggle();
+      this.isCollapsed = false;
+    } else {
+      this.sidenav.open();
+      this.isCollapsed = !this.isCollapsed;
+    }
+  }
+
+  navigateMenu(menu: string) {
+    this.router.navigate([menu])
+  }
+}
